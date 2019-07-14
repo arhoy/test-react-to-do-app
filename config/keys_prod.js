f8f1e0f682bb2d673d8885beb8dab2ddfaea4e1b
aquasar <arhoy@ualberta.ca>
2019-07-14 14:06:45 -0600

    with full mongoose driver

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -5,4 +5,4 @@ module.exports = {
 }
 
 
-// mongodb://alex:<password>@todoapp-prod-shard-00-00-eik2v.mongodb.net:27017,todoapp-prod-shard-00-01-eik2v.mongodb.net:27017,todoapp-prod-shard-00-02-eik2v.mongodb.net:27017/test?ssl=true&replicaSet=todoapp-prod-shard-0&authSource=admin&retryWrites=true&w=majority
+// see notes
