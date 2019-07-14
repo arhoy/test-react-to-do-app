b7d060186974685dd2822246bd3989e48b4df0b9
aquasar <arhoy@ualberta.ca>
2019-07-14 14:06:01 -0600

    with full mongoose driver

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -3,3 +3,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY
 }
+
+
+// mongodb://alex:<password>@todoapp-prod-shard-00-00-eik2v.mongodb.net:27017,todoapp-prod-shard-00-01-eik2v.mongodb.net:27017,todoapp-prod-shard-00-02-eik2v.mongodb.net:27017/test?ssl=true&replicaSet=todoapp-prod-shard-0&authSource=admin&retryWrites=true&w=majority
